776e7f4afc2c65566f1de8e438b7d6207c7abd14
NF11 <nassim.fares.dz@gmail.com>
2020-07-21 13:10:24 +0100

    add test for login singup fetch_one delete

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,13 +1,106 @@
 const superTest = require("supertest");
 const app = require("../src/app");
+const User = require("../src/models/user");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+
+const userOneID = new mongoose.Types.ObjectId();
+
+const userOne = {
+  _id: userOneID,
+  name: "test",
+  email: "test@test.com",
+  password: "testtest",
+  tokens: [
+    {
+      token: jwt.sign({ _id: userOneID }, process.env.JWT_KEY),
+    },
+  ],
+};
+
+beforeEach(async () => {
+  await User.deleteMany();
+  await new User(userOne).save();
+});
 
 test("Should singup a new user", async () => {
-  await superTest(app)
+  const response = await superTest(app)
     .post("/users/signup")
     .send({
       name: "nassim",
-      email: "dsqdqsdqds@gdffd.com",
+      email: "nassim@nassim.com",
       password: "passtoto",
     })
     .expect(201);
+  // Assert that the database was changed correctly
+  const user = await User.findById(response.body.user._id);
+  expect(user).not.toBeNull();
+
+  // Assert about the response
+  expect(response.body).toMatchObject({
+    user: {
+      name: "nassim",
+      email: "nassim@nassim.com",
+    },
+    token: user.tokens[0].token,
+  });
+  //  TODO Assert about password is encrypted
+});
+
+test("Should login existing user", async () => {
+  const response = await superTest(app)
+    .post("/users/login")
+    .send({
+      email: userOne.email,
+      password: userOne.password,
+    })
+    .expect(200);
+  const user = await User.findById(response.body.user._id);
+  await expect(user).not.toBeNull();
+  await expect(user.tokens[1].token).toMatch(userOne.tokens[0].token);
+});
+test("Should not login no existing user", async () => {
+  await superTest(app)
+    .post("/users/login")
+    .send({
+      email: "userOne.email",
+      password: "userOne.password",
+    })
+    .expect(400);
+});
+
+test("Should get profile for user", async () => {
+  await superTest(app)
+    .get("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+});
+test("Should get profile for user whit bad token", async () => {
+  await superTest(app)
+    .get("/users/me")
+    .set("Authorization", `Bearer 231sd561fs1cfsd1f56s1f5s156sfd1sf15d6`)
+    .send()
+    .expect(401);
+});
+test("Should not get profile for unauthenticated user", async () => {
+  await superTest(app).get("/users/me").send().expect(401);
+});
+
+test("Should delete account for authenticated user", async () => {
+  const response = await superTest(app)
+    .delete("/users/me")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+  //  Assert user is removed
+  const user = await User.findById(userOneID);
+  expect(user).toBeNull();
+});
+test("Should not delete account for unauthenticated user", async () => {
+  await superTest(app)
+    .delete("/users/me")
+    .set("Authorization", `Bearer 231sd561fs1cfsd1f56s1f5s156sfd1sf15d6`)
+    .send()
+    .expect(401);
 });
